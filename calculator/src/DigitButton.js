2188b25f638a024f852de6de367bd26afa1d1098
andrescn2020 <andrescn_2010@hotmail.com>
2022-07-17 15:01:35 -0300

    Finish

---
+++ calculator/src/DigitButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ACTIONS } from './App';
+
+export const DigitButton = ( { dispatch, digit} ) => {
+
+  return (
+
+    <button onClick={() => dispatch( { type: ACTIONS.ADD_DIGIT, payload: { digit } })}>{digit}</button>
+  
+    )
+
+}
+
+export default DigitButton;
